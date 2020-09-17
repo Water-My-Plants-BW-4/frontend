f8ea9391247d657ad93e7d1455317766098d842e
bhawnishk1 <mrbhawnish@gmail.com>
2020-09-16 19:03:28 -0700

    PrivateRoute set up with PlantPage HOC as the HOC component

---
+++ water-my-plants/src/components/PlantPage.js
@@ -0,0 +1,24 @@
+//This component will be the parent component 
+//which will display PlantList component as well as the AddMyPlant component
+import React, {useState, useEffect } from "react";
+import axios from "axios";
+
+//useState is just added as a placeholder, as we will implement context API
+
+
+const PlantPage = () => {
+  const [loading, setLoading] = useState(false)
+  const [plantsList, setPlantList] = useState([])
+
+//------FRONT-END I will work here--------
+
+    return(
+     <>
+     {/* plantlist component will be added here
+     <PlantList component></PlantList>
+      */}
+    </>
+    )
+}
+
+export default PlantPage;
