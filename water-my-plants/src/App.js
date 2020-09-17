5208f62ba0acbfbcddcac0809e6bb9a8aafb2275
Jorge Jimenez <calenocam24@gmail.com>
2020-09-17 18:42:15 -0400

    added info to axiosWithAuth, and App.  Created Header for NavLinks

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Header from "./components/Header";
+import { UserContext } from "./context/UserContext";
+import AddPlantsForm from "./components/AddPlantsForm";
+import UserForm from "./components/UserForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <Header />
+        <UserContext.Provider>
+          <PrivateRoute exact path="/userInfo" component={UserForm} />
+          <PrivateRoute exact path="/addPlants" component={AddPlantsForm} />
+        </UserContext.Provider>
+      </>
     </div>
   );
 }
