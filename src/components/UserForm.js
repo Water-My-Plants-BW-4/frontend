1d277e58f46095b40dad247c80df621db623e76b
Jorge A Jimenez <57850031+Caleno83@users.noreply.github.com>
2020-09-24 20:32:27 -0400

    Update UserForm.js

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import axiosWithAuth from "../utils/axiosWithAuth";
 import UserCard from "../components/UserCard";
 import { UserContext } from "../context/UserContext";
